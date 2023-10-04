ffae5c4f981c388dff19c7ff6faa2814c6276b18
fatimaz101 <fatimayasin100@gmail.com>
2023-10-03 22:42:20 -0500

    Init

---
+++ MusicLibraryWebAPI/Controllers/SongsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicLibraryWebAPI.Data;
+using MusicLibraryWebAPI.Models;
+using System.Runtime.Versioning;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicLibraryWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    
+    public class SongsController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        public SongsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<SongsController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            var songs = _context.Songs.ToList();
+
+             return Ok(songs);
+        }
+
+        // GET api/<SongsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SongsController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Song song)
+        {
+
+            _context.Add(song);
+            _context.SaveChanges();
+            return StatusCode(201, song);
+        }
+
+        // PUT api/<SongsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SongsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MusicLibraryWebAPI/Models/Song.cs
@@ -0,0 +1,23 @@
+﻿namespace MusicLibraryWebAPI.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+
+
+
+
+        //        Id - int
+        //Title - string
+        //Artist - string
+        //Album - string
+        //ReleaseDate - DateTime
+        //Genre - string
+    }
+}
